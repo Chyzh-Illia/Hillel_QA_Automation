e587798c67f892a90502de7f9f5e72106dd92620
Chyzh-Illia <chyzh.illia@gmail.com>
2024-11-15 09:58:58 +0100

    Added 2 task

---
+++ Lessons/Lesson_06/Lesson_06_02/Homework_06_02.py
@@ -0,0 +1,5 @@
+incoming_data = str(input("Enter your the 'h' word: ")) # Input any data
+
+while incoming_data != "h":
+    incoming_data = str(input("Enter the 'h' word if you wand to exit from code, or it will be infinity :) :"))
+    print(f"Your output: {incoming_data}")

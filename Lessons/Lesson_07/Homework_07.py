8726a74427d2776e8eaf7664e7198f14297f3d05
Chyzh-Illia <chyzh.illia@gmail.com>
2024-11-18 17:26:46 +0100

    Added fix text

--- Lessons/Lesson_07/Homework_07.py
+++ Lessons/Lesson_07/Homework_07.py
@@ -94,7 +94,7 @@ def calculate(number1: int | float, number2: int | float, operation="+"):
     elif operation == "*":
         return number1 * number2
     elif number2 == 0:
-        return f"Enter correct number, we can't devide to 0"
+        return f"Enter correct number, we can't devide or other methods to 0"
     elif operation == "/":
         return number1 / number2
     elif operation == "**":

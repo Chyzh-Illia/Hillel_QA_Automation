723c2852b252899a5371d9df96c1e28e9f0e0450
Chyzh-Illia <chyzh.illia@gmail.com>
2024-11-27 14:39:00 +0100

    Added lastest tasks Lesson_09

--- Lessons/Lesson_09/Test_Homework_09.py
+++ Lessons/Lesson_09/Test_Homework_09.py
@@ -2,13 +2,15 @@ import unittest
 import sys
 import pathlib
 
+from Lessons.Lesson_07.Homework_07 import split_str
+
+sys.path.insert(0, str(pathlib.Path(__file__).parent.parent))
+
 from Lessons.Lesson_09.Def_calculation_percentage import calc_percentage
 from Lessons.Lesson_09.Def_from_HW_07_01 import arithmetic_mean
 from Lessons.Lesson_09.Def_from_HW_07_02 import calculate
 from Lessons.Lesson_09.Def_from_HW_07_03 import sum_numbers
 from Lessons.Lesson_09.Def_from_HW_07_05 import verify_on_word
-
-sys.path.insert(0, str(pathlib.Path(__file__).parent.parent))
 
 class arithmetic_Test(unittest.TestCase):
     def test_arithmetic_mean(self):
@@ -16,12 +18,6 @@ class arithmetic_Test(unittest.TestCase):
         actual_result = arithmetic_mean(2, 10)
 
         self.assertEqual(expected_result, actual_result)
-
-    def test_calculate(self):
-        expected_result = 25
-        actual_result = calculate(5, 2, "**")
-
-        self.assertEqual(actual_result, expected_result)
 
     def test_sum_numbers(self):
         expected_result = 100
@@ -39,6 +35,41 @@ class arithmetic_Test(unittest.TestCase):
         expected_result = True
         actual_result = verify_on_word("Hello world")
 
+        self.assertEqual(actual_result, expected_result)
+
+class calculate_Test(unittest.TestCase):
+    def test_calculate_exponentiation(self):
+        expected_result = 25
+        actual_result = calculate(5, 2, "**")
+
+        self.assertEqual(actual_result, expected_result)
+
+    def test_calculate_sum_division_on_zero(self):
+        expected_result = "Enter correct number, we can't devide or other methods to 0"
+        actual_result = calculate(5, 0, "//")
+
+        self.assertEqual(actual_result, expected_result)
+
+    def test_calculate_multiply_numbers(self):
+        expected_result = 1000
+        actual_result = calculate(5, 200, "*")
+
+        self.assertEqual(actual_result, expected_result)
+        print(f"\n result_after_multiply: {actual_result}")
+
+    def test_calculate_sum_numbers(self):
+        expected_result = 50
+        actual_result = calculate(6, 44, "+")
+        self.assertEqual(actual_result, expected_result)
+
+    def test_calculate_division(self):
+        expected_result = 3
+        actual_result = calculate(9, 3, "/")
+        self.assertEqual(actual_result, expected_result)
+
+    def test_split_text(self):
+        expected_result = ["Test", "data", "here"]
+        actual_result = split_str("Test data here")
         self.assertEqual(actual_result, expected_result)
 
 if __name__ == "__main__":

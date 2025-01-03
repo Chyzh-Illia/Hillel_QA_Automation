c897d16027aaf4bc712f13816c268c8d27c55ff6
Chyzh-Illia <chyzh.illia@gmail.com>
2025-01-03 13:49:05 +0100

    Fixed error message

--- Lessons/Lesson_16/Homework_16_1.py
+++ Lessons/Lesson_16/Homework_16_1.py
@@ -18,17 +18,19 @@ class Developer(Employee):
 
 class TeamLead(Manager, Developer):
     def __init__(self, name, salary, department, programming_language, team_size):
-        Manager.__init__(self, name, salary, department)
-        Developer.__init__(self, name, salary, programming_language)
+        # Используем super() для MRO
+        super(TeamLead, self).__init__(name, salary, department)
+        self.programming_language = programming_language
         self.team_size = team_size
+
 
 def test_teamlead():
     team_lead = TeamLead("Alex", 100000, "IT", "Python", 5)
-    assert hasattr(team_lead, "name"), "Atribute 'name' exist"
-    assert hasattr(team_lead, "salary"), "Atribute 'salary' exist"
-    assert hasattr(team_lead, "department"), "Atribute 'department' exist"
-    assert hasattr(team_lead, "programming_language"), "Atribute 'programming_language' відсутній"
-    assert hasattr(team_lead, "team_size"), "Atribute 'team_size' exist"
+    assert hasattr(team_lead, "name"), "Attribute 'name' does not exist"
+    assert hasattr(team_lead, "salary"), "Attribute 'salary' does not exist"
+    assert hasattr(team_lead, "department"), "Attribute 'department' does not exist"
+    assert hasattr(team_lead, "programming_language"), "Attribute 'programming_language' does not exist"
+    assert hasattr(team_lead, "team_size"), "Attribute 'team_size' does not exist"
     print("Test passed, complete!")
 
 test_teamlead()

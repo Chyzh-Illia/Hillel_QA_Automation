cbc11faee1784ce1956b98c1f900a061f7f8403c
Chyzh-Illia <chyzh.illia@gmail.com>
2025-02-01 19:16:35 +0100

    Lesson_13.1

--- Lessons/Lesson_13_1/Homework_13_1.py
+++ Lessons/Lesson_13_1/Homework_13_1.py
@@ -4,17 +4,13 @@ import json
 import logging
 import xml.etree.ElementTree as ET
 
-# Настройка логгера
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger()
 
-# Укажите свою фамилию
 second_name = "Chyzh"
 
-# Определяем путь к текущей директории
 base_path = os.path.dirname(__file__)
 
-# --- Задание 1: Работа с CSV ---
 def remove_duplicates_from_csv(file1, file2, output_file):
     seen_rows = set()
     result = []
@@ -35,11 +31,10 @@ def remove_duplicates_from_csv(file1, file2, output_file):
         writer = csv.writer(f)
         writer.writerows(result)
 
-    logger.info(f"Дубликаты удалены, результат сохранён в {output_file}")
+    logger.info(f"Duplicates were removed, the result was saved in {output_file}")
 
 remove_duplicates_from_csv("random.csv", "rmc.csv", f"result_{second_name}.csv")
 
-# --- Задание 2: Валидация JSON ---
 def validate_json_files(log_file):
     for filename in os.listdir(base_path):
         if filename.endswith(".json"):
@@ -48,13 +43,12 @@ def validate_json_files(log_file):
                 with open(file_path, 'r', encoding='utf-8') as f:
                     json.load(f)
             except json.JSONDecodeError as e:
-                logger.error(f"Файл {filename} невалиден: {e}")
+                logger.error(f"File {filename} not validation: {e}")
                 with open(os.path.join(base_path, log_file), 'a', encoding='utf-8') as log_f:
-                    log_f.write(f"Файл {filename} невалиден: {e}\\n")
+                    log_f.write(f"File {filename} not validation: {e}\\n")
 
 validate_json_files(f"json__{second_name}.log")
 
-# --- Задание 3: Поиск в XML ---
 def find_incoming_by_group_number(xml_file, group_number):
     tree = ET.parse(os.path.join(base_path, xml_file))
     root = tree.getroot()
@@ -66,10 +60,10 @@ def find_incoming_by_group_number(xml_file, group_number):
             if timing is not None:
                 incoming = timing.find("incoming")
                 if incoming is not None:
-                    logger.info(f"Значение incoming для group {group_number}: {incoming.text}")
+                    logger.info(f"Value incoming for group {group_number}: {incoming.text}")
                     return incoming.text
 
-    logger.info(f"Group {group_number} не найдено или нет incoming.")
+    logger.info(f"Group {group_number} not found or not incoming.")
     return None
 
 find_incoming_by_group_number("groups.xml", 2)
